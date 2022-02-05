c04612ea04a67f404970f5a21d6b4af701d20a9a
Shawn207 <60746163+Shawn207@users.noreply.github.com>
2022-02-05 17:13:20 -0500

    record certain amount of time in cv mode

--- capture_video_cv.py
+++ capture_video_cv.py
@@ -4,8 +4,7 @@
 
 import sys
 sys.path.append("./AirSim/PythonClient/computer_vision")
-from pynput import keyboard
-
+# from pynput import Key, Listener, KeyCode
 import setup_path
 import airsim
 
@@ -15,13 +14,16 @@ import time
 import math
 import tempfile
 
+
+
+
 pp = pprint.PrettyPrinter(indent=4)
 
 client = airsim.VehicleClient()
 client.confirmConnection()
 
 airsim.wait_key('Press any key to set camera-0 gimbal to -15-degree pitch')
-camera_pose = airsim.Pose(airsim.Vector3r(0, 0, 0), airsim.to_quaternion(math.radians(-15), 0, 0)) #radians
+camera_pose = airsim.Pose(airsim.Vector3r(0, 0, -9), airsim.to_quaternion(math.radians(-15), 0, 0)) #radians
 client.simSetCameraPose("0", camera_pose)
 
 airsim.wait_key('Press any key to get camera parameters')
@@ -37,33 +39,44 @@ except OSError:
     if not os.path.isdir(tmp_dir):
         raise
 
-airsim.wait_key('Press any key to get images')
-all_responses = []
-while client.simIsPause():
-    # z = x * -20 - 5 # some random number
-    # client.simSetVehiclePose(airsim.Pose(airsim.Vector3r(z, z, z), airsim.to_quaternion(x / 3.0, 0, x / 3.0)), True)
+video_time = int(input("How many seconds do you want to record?"))
 
+airsim.wait_key('Press any key to get images')
+time.sleep(2)
+start = time.time()
+print("start time: ",start, "s")
+end = start
+all_responses = []
+
+while end-start < video_time:
     responses = client.simGetImages([
         airsim.ImageRequest("0", airsim.ImageType.DepthVis),
         airsim.ImageRequest("0", airsim.ImageType.Segmentation),
         airsim.ImageRequest("0", airsim.ImageType.Scene)])
-        # airsim.ImageRequest("4", airsim.ImageType.DisparityNormalized),
-        # airsim.ImageRequest("4", airsim.ImageType.SurfaceNormals)])
     all_responses.append(responses)
+    end = time.time()
+    print("a frame has been captured, time: ", end, "s")
 
 print(">>>>>>>>>>>>>>>>>>>>>>>The recording has been stopped<<<<<<<<<<<<<<<<<<<<<<<<<")
+if not os.path.isdir(os.path.join(tmp_dir,"type0")):
+    os.mkdir(os.path.join(tmp_dir,"type0"))
+if not os.path.isdir(os.path.join(tmp_dir,"type1")):
+    os.mkdir(os.path.join(tmp_dir,"type1"))
+if not os.path.isdir(os.path.join(tmp_dir,"type2")):
+    os.mkdir(os.path.join(tmp_dir,"type2"))
 for i, responses in enumerate(all_responses):
+    filename = [os.path.join(tmp_dir,"type0", "frame_" + str(i) +"_type_0"),os.path.join(tmp_dir,"type1", "frame_" + str(i) +"_type_1"),os.path.join(tmp_dir,"type2", "frame_" + str(i) +"_type_2")]
     for j,response in enumerate(responses):
-        filename = os.path.join(tmp_dir, "frame_" + str(i), j)
+        
         if response.pixels_as_float:
             # print("Type %d, size %d, pos %s" % (response.image_type, len(response.image_data_float), pprint.pformat(response.camera_position)))
-            airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
+            airsim.write_pfm(os.path.normpath(filename[j] + '.pfm'), airsim.get_pfm_array(response))
         else:
             # print("Type %d, size %d, pos %s" % (response.image_type, len(response.image_data_uint8), pprint.pformat(response.camera_position)))
-            airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
+            airsim.write_file(os.path.normpath(filename[j] + '.png'), response.image_data_uint8)
 
         pose = client.simGetVehiclePose()
-        pp.pprint(pose)
+        # pp.pprint(pose)
 
         # time.sleep(3)
 print(">>>>>>>>>>>>>>>>>>>>>>>The images are saved now<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
