2d83198513e70a37afae74b255cc6ffab96b9bf6
Jasper-Y <chengjiyu.sjtu@gmail.com>
2022-02-14 18:30:25 -0500

    feat: modify randomization

--- capture_static_image.py
+++ capture_static_image.py
@@ -2,6 +2,8 @@
 # https://github.com/Microsoft/AirSim/blob/master/docs/image_apis.md#computer-vision-mode
 # This is modified based on cv_mode.py
 
+from operator import index
+from random import shuffle
 import sys
 import airsim
 
@@ -46,6 +48,7 @@ client = airsim.VehicleClient()
 client.confirmConnection()
 
 camera_name = "0"
+object_index = 1
 object_name = "SM_ForkLift5"
 file_deli = '\\'
 
@@ -54,6 +57,8 @@ file_deli = '\\'
 
 time_str = datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M")
 save_folder_path = os.path.abspath("data") + file_deli + time_str
+os.mkdir(save_folder_path)
+save_folder_path += f'{object_index}'
 os.mkdir(save_folder_path)
 os.mkdir(save_folder_path + file_deli + 'depth')
 os.mkdir(save_folder_path + file_deli + 'rgb')
@@ -88,72 +93,101 @@ def extract_save_image(cnt):
 
 
 airsim.wait_key(
-    'Press any key to set camera-0 gimbal to -15-degree pitch and start loop')
-camera_pose = airsim.Pose(airsim.Vector3r(
-    0, 0, -9), airsim.to_quaternion(math.radians(-15), 0, 0))  # radians
-client.simSetCameraPose(camera_name, camera_pose)
-
-time.sleep(1)
-embed()
+    'Press any key to start loop')
 
 cnt = 0
-exit_flag = True
+exit_flag = False
+shuffle_flag = True
 
-vehicle_pos = np.array([9., 8., -1.])
-# vehicle_ori = [0, 0, 0, 1]
-vehicle_angle = np.array([0., 0., 0.])
-camera_pos = np.array([3., 3., -10.])
-# camera_ori = [0, -0.3826, 0, 0.9238]
-camera_angle = np.array([0., -np.pi / 4, 0.])
-while exit_flag:
-    # TODO: generate random pose here
-    vehicle_angle += np.array([0., 0., np.pi / 10])
+# vehicle_pos = np.array([9., 8., -1.])
+# # vehicle_ori = [0, 0, 0, 1]
+# vehicle_angle = np.array([0., 0., 0.])
+# camera_pos = np.array([3., 3., -10.])
+# # camera_ori = [0, -0.3826, 0, 0.9238]
+# camera_angle = np.array([0., -np.pi / 4, 0.])
 
-    # [w x y z]
-    vehicle_ori = tfm.euler.euler2quat(*vehicle_angle, 'rxyz')
-    camera_ori = tfm.euler.euler2quat(*camera_angle, 'rxyz')
+camera_num = 2
+vehicle_num = 10
+index_list = np.array([x for x in range(camera_num * vehicle_num)])
+if shuffle_flag:
+    np.random.shuffle(index_list)
+base2center = np.array(
+    [[1., 0., 0., 10.], [0., 1., 0., 10.], [0., 0., 1., -1.], [0., 0., 0., 1.]])
 
-    # airsim does NOT accept numpy.float64
-    vehicle_pos_ls = vehicle_pos.tolist()
-    vehicle_ori_ls = vehicle_ori.tolist()
-    camera_pos_ls = camera_pos.tolist()
-    camera_ori_ls = camera_ori.tolist()
+embed()
 
-    print(vehicle_ori_ls)
-
-    object_pose = airsim.Pose(airsim.Vector3r(vehicle_pos_ls[0], vehicle_pos_ls[1], vehicle_pos_ls[2]), airsim.Quaternionr(
-        vehicle_ori_ls[1], vehicle_ori_ls[2], vehicle_ori_ls[3], vehicle_ori_ls[0]))
-    if not client.simSetObjectPose(object_name, object_pose):
-        print("Failed to set vehicle pose, check if you set the object mobility to movable! Quit...")
+# random cam
+for i in range(camera_num):
+    if exit_flag:
         break
+    center_cam_ori_x = 0
+    center_cam_ori_y = np.random.uniform() * np.pi * -0.4
+    center_cam_ori_z = np.random.uniform() * np.pi * 2
 
-    camera_pose = airsim.Pose(airsim.Vector3r(camera_pos_ls[0], camera_pos_ls[1], camera_pos_ls[2]), airsim.Quaternionr(
-        camera_ori_ls[1], camera_ori_ls[2], camera_ori_ls[3], camera_ori_ls[0]))
-    client.simSetCameraPose(camera_name, camera_pose)
-    time.sleep(1)
+    center_cam_ori = tfm.euler.euler2mat(
+        center_cam_ori_z, center_cam_ori_y, center_cam_ori_x, "rzyx")
+    dis = np.random.uniform() * 5 + 5
+    # make the x axis of the camera pointing to the center
+    center_cam_pos = -dis * np.expand_dims(center_cam_ori[:, 0], 1)
+    center2cam = np.concatenate(
+        [np.concatenate([center_cam_ori, center_cam_pos], 1), [[0, 0, 0, 1]]], 0)
 
-    actual_obj_pose = airsimPoseToMat(client.simGetObjectPose(object_name))
-    actual_cam_pose = airsimPoseToMat(
-        client.simGetCameraInfo(camera_name).pose)
+    base2cam = base2center @ center2cam
+    camera_quat = tfm.quaternions.mat2quat(
+        base2cam[:3, :3]).tolist()  # [w, x, y, z]
+    camera_posi = base2cam[:3, 3].tolist()
+    # airsim.Quaternionr accepts [x, y, z, w]
+    airsim_camera_pose = airsim.Pose(airsim.Vector3r(camera_posi[0], camera_posi[1], camera_posi[2]),
+                                     airsim.Quaternionr(camera_quat[1], camera_quat[2], camera_quat[3], camera_quat[0]))
+    client.simSetCameraPose(camera_name, airsim_camera_pose)
+    print(f"Setting camera pose {i}: {base2cam}")
+    time.sleep(5)
 
-    print("\n\nPress Y to save current scene, N to disgard, Q to finish:\n")
-
-    while True:
-        k = keyboard.read_key()
-        if k == 'N' or k == 'n':
-            print(f"Discard #{cnt} data\n")
+    for j in range(vehicle_num):
+        x_displace = np.random.uniform() * 0
+        y_displace = np.random.uniform() * 0
+        rz = np.random.uniform() * np.pi * 2
+        # [w, x, y, z]
+        center_vehicle_ori = tfm.euler.euler2quat(rz, 0, 0, "rzyx").tolist()
+        vehicle_posi = base2center[:3, 3].tolist()
+        airsim_object_pose = airsim.Pose(airsim.Vector3r(vehicle_posi[0] + x_displace, vehicle_posi[1] + y_displace, vehicle_posi[2]),
+                                         airsim.Quaternionr(center_vehicle_ori[1], center_vehicle_ori[2], center_vehicle_ori[3], center_vehicle_ori[0]))
+        if not client.simSetObjectPose(object_name, airsim_object_pose):
+            print(
+                "Failed to set vehicle pose, check if you set the object mobility to movable! Quit...")
+            exit_flag = True
             break
-        elif k == 'Y' or k == 'y':
-            extract_save_image(cnt)
-            cam_wrt_obj = np.linalg.inv(actual_obj_pose) @ actual_cam_pose
-            np.savetxt(
-                f"{save_folder_path}{file_deli}pose{file_deli}{cnt}.txt", cam_wrt_obj)
-            print(f"Save #{cnt} data\n")
-            cnt += 1
-            break
-        elif k == 'Q' or k == 'q':
-            exit_flag = False
-            break
+        time.sleep(1)
+
+        actual_obj_pose = airsimPoseToMat(client.simGetObjectPose(object_name))
+        actual_cam_pose = airsimPoseToMat(
+            client.simGetCameraInfo(camera_name).pose)
+
+        extract_save_image(index_list[cnt])
+        cam_wrt_obj = np.linalg.inv(actual_obj_pose) @ actual_cam_pose
+        np.savetxt(
+            f"{save_folder_path}{file_deli}pose{file_deli}{index_list[cnt]}.txt", cam_wrt_obj)
+        print(f"Save #{cnt} data with index {index_list[cnt]}\n")
+        cnt += 1
+
+        # print("\n\nPress Y to save current scene, N to disgard, Q to finish:\n")
+
+        # while True:
+        #     k = keyboard.read_key()
+        #     if k == 'N' or k == 'n':
+        #         print(f"Discard #{cnt} data\n")
+        #         break
+        #     elif k == 'Y' or k == 'y':
+        #         extract_save_image(cnt)
+        #         cam_wrt_obj = np.linalg.inv(actual_obj_pose) @ actual_cam_pose
+        #         np.savetxt(
+        #             f"{save_folder_path}{file_deli}pose{file_deli}{cnt}.txt", cam_wrt_obj)
+        #         print(f"Save #{cnt} data\n")
+        #         cnt += 1
+        #         break
+        #     elif k == 'Q' or k == 'q':
+        #         exit_flag = False
+        #         break
 
 
 embed()
